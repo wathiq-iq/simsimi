2d14522756a395082090a79c212ba2700d8887d4
Wathiq Al-Qajar <wathiq-iq@users.noreply.github.com>
2016-07-21 03:33:29 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,17 +4,18 @@ return {
 	bot_api_key = '',
 	-- Your Telegram ID.
 	admin = ,
+	-- about the bot
 	text = [[
-	
+	<text hear>
 	]],
-     
-	simsimi_key = '',
+        -- Your authorization token Api from the http://developer.simsimi.com/
+	simsimi_key = '41250a68-3cb5-43c8-9aa2-d7b3caf519b1',
+	-- lang 45 
 	lang = 'ar','en','af','bg','ca','ch','cs','cy','da','de','el','es','eu','fi','fr','he','hi','hr','hu','id','it','ja','kh','ko','lt','ml','ms','nb','nl','pa','ph','pl','pt','ro','rs','ru','sk','sv','ta','te','th','tr','uk','vn','zh',
-	
+	-- true or false
 	langer = true,
-	
 	errors = {connection = 'Connection error.',results = 'No results found.',argument = 'Invalid argument.',syntax = 'Invalid syntax.',chatter_connection = 'I don\'t feel like talking right now.',chatter_response = 'I don\'t know what to say to that.'},
-
+        -- Plugins 
 	plugins = {
 	'simsimi',
 	'help',
